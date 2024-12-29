8c7aec5a6b2c6ddf24f42d82deb6bd1df235248a
Gaurang2712 <gaurangchaudhary2712@gmail.com>
2024-12-29 20:42:07 +0530

    first commit

---
+++ 1.py
@@ -0,0 +1,18 @@
+#single line comment
+"""
+this is multiple line comment 
+"""
+
+# Write a program to perform input/output of all basic data types.
+
+a=1
+print(type(a))  #int data type
+
+b="Gaurang"
+print(type(b)) #str data type
+
+c=1.20
+print(type(c))
+
+d=1j
+print(type(d))
